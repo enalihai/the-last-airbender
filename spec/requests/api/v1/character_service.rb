088f0b5a466adbf7946639cdb110b54bc94b0984
LucasPascale <lpluke5@gmail.com>
2022-07-22 10:38:31 -0500

    touching the api and working on next steps

---
+++ spec/requests/api/v1/character_service.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "the airbender request" do
+    describe 'by Nation returns'
+        it 'characters by Nation'
+        it 'total number in the Nation'
+        it 'displays detailed info on the first 25 members'
+    end
+end
