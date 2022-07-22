3922d07af359ee970d7d0bea608cf8928f56061e
LucasPascale <lpluke5@gmail.com>
2022-07-22 12:33:00 -0500

    had trouble, was on my wrong branch

--- app/services/characters_service.rb
+++ app/services/characters_service.rb
@@ -2,12 +2,11 @@ class CharactersService
     def self.characters_by_nation(nation)
         response = conn.get('/api/v1/characters') do |faraday|
             faraday.params['affiliation'] = nation
+            faraday.params['perPage'] = 100
+            faraday.params['page']
         end
         JSON.parse(response.body, symbolize_names: true)
     end
-
-    # def self.get_long_list(nation)
-    # end
 
     def self.conn
         Faraday.new(url: "http://last-airbender-api.herokuapp.com")
